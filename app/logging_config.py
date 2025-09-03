b08b40c77c409e3a46fe7269eb089f6e577cf9c7
Kirthan N B <kirthannb@gmail.com>
2025-09-03 22:16:51 +0530

    Add files via upload

--- app/logging_config.py
+++ app/logging_config.py
@@ -2,11 +2,19 @@ import logging
 import sys
 
 def setup_logging():
+    # Create a handler that can handle Unicode
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    
+    # Set encoding to UTF-8 for the handler
+    try:
+        handler.stream.reconfigure(encoding='utf-8')
+    except:
+        pass  # Some streams may not support reconfigure
+    
+    # Configure root logger
     logging.basicConfig(
         level=logging.INFO,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        handlers=[
-            logging.StreamHandler(sys.stdout),
-            logging.FileHandler('app.log')
-        ]
+        handlers=[handler],
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     )
